a100ca3847bd57bb74f133bc54d30963f3d6b5de
ZWW <951598077@qq.com>
2019-05-28 21:31:06 +0800

    无缝无缝轮播

---
+++ main.js
@@ -0,0 +1,55 @@
+// setTimeout(function(){
+//     $('.images>img:nth-child(1)').css({
+//         transform:'translateX(-100%)'
+//     })
+//     $('.images>img:nth-child(2)').css({
+//         transform:'translateX(-100%)'
+//     })
+//     $('.images>img:nth-child(1)').one('transitionend',function(e){
+//         $(e.currentTarget).addClass('right').css({transform:'none'})
+//     })
+// },3000)
+
+// setTimeout(function(){
+//     $('.images>img:nth-child(2)').css({
+//         transform:'translateX(-200%)'
+//     })
+//     $('.images>img:nth-child(3)').css({
+//         transform:'translateX(-100%)'
+//     })
+//     $('.images>img:nth-child(2)').one('transitionend',function(e){
+//         $(e.currentTarget).addClass('right').css({transform:'none'})
+//     })
+// },6000)
+
+// setTimeout(function(){
+//     $('.images>img:nth-child(3)').css({
+//         transform:'translateX(-200%)'
+//     })
+//     $('.images>img:nth-child(1)').css({
+//         transform:'translateX(-100%)'
+//     })
+//     $('.images>img:nth-child(3)').one('transitionend',function(e){
+//         $(e.currentTarget).addClass('right').css({transform:'none'})
+//     })
+// },9000)
+
+// setTimeout(function(){
+//     $('.images>img:nth-child(1)').css({
+//         transform:'translateX(-200%)'
+//     })
+//     $('.images>img:nth-child(2)').css({
+//         transform:'translateX(-100%)'
+//     })
+//     $('.images>img:nth-child(1)').one('transitionend',function(e){
+//         $(e.currentTarget).addClass('right').css({transform:'none'})
+//     })
+// },12000)
+
+
+$('.images > img:nth-child(1)').addClass('current')
+$('.images > img:nth-child(1)').addClass('enter')
+setTimeout(()=>{
+    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
+    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
+})
