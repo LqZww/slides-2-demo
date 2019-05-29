9e08f7c7b49f7a9b0a5f059e1d78b8e8d07b4224
ZWW <951598077@qq.com>
2019-05-29 18:03:49 +0800

    完善无缝轮播

--- main.js
+++ main.js
@@ -46,10 +46,46 @@
 //     })
 // },12000)
 
+let n
+initialise()
+setInterval(() => {
+    makeLeave(getImage(n))
+        .one('transitionend', (e) => {
+            makeEnter($(e.currentTarget))
+        })
+    makeCurrent(getImage(n+1))
+    n += 1
+}, 3000)
 
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(1)').addClass('enter')
-setTimeout(()=>{
-    $('.images > img:nth-child(1)').removeClass('current').addClass('leave')
-    $('.images > img:nth-child(2)').removeClass('enter').addClass('current')
-})
+
+function getImage(n){
+    return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n) {
+    if (n > 3) {
+        n = n % 3
+        if (n === 0) {
+            n = 3
+        }
+    }
+    return n;
+}
+
+function initialise() {
+    n = 1
+    $(`.images > img:nth-child(${n})`).addClass('current')
+        .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+    return $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave($node){
+    return $node.removeClass('enter current').addClass('leave')
+}
+
+function makeEnter($node){
+    return $node.removeClass('leave current').addClass('enter')
+}
